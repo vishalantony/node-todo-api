a4b594ce397e3c2fc14465335b6d4b2499ababd8
Vishal Antony <vishalantony@outlook.com>
2017-08-27 00:15:01 +0530

    testing name change

--- playground/mongodb-find.js
+++ playground/mongodb-find.js
@@ -22,7 +22,7 @@ MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, db) => {
   //   console.log('Unable to fetch Todo', err);
   // });
 
-  db.collection('Users').find({name: 'Catherine George'}).toArray().then((docs) => {
+  db.collection('Users').find({name: 'Vishal Antony'}).toArray().then((docs) => {
     console.log('Users');
     console.log(JSON.stringify(docs, undefined, 2));
   }, (err) => {
