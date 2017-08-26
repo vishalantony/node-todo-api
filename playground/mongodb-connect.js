cc501886cf77f7a4b314f4b7762c1db30d28f68f
Vishal Antony <vishalantony@outlook.com>
2017-08-27 00:00:57 +0530

    Initial commit

---
+++ playground/mongodb-connect.js
@@ -0,0 +1,30 @@
+const { MongoClient, ObjectID } = require('mongodb');
+
+MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, db) => {
+  if(err) {
+    return console.log('Unable to connect to the DB', err);
+  }
+  console.log('Connected to MongoDB server');
+  db.collection('Todos').insertOne({
+    text: 'Something to do again and again and again',
+    completed: false
+  }, (err, res) => {
+    if(err) {
+      return console.log('Unable to insert Todo to the DB', err);
+    }
+    console.log(JSON.stringify(res.ops, undefined, 2));
+  });
+
+  // db.collection('Users').insertOne({
+  //   name: 'Mario Sukamoto',
+  //   age: 29,
+  //   location: 'Tokyo'
+  // }, (err, res) => {
+  //   if(err) {
+  //     console.log('Unable to insert user', res);
+  //   }
+  //   console.log(res.ops[0]._id.getTimestamp());
+  // });
+  //
+  db.close();
+});
---
+++ playground/mongodb-find.js
@@ -0,0 +1,33 @@
+const { MongoClient, ObjectID } = require('mongodb');
+
+MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, db) => {
+  if(err) {
+    return console.log('Unable to connect to the DB', err);
+  }
+  console.log('Connected to MongoDB server');
+
+  // db.collection('Todos').find({
+  //   _id: new ObjectID('59a0418be414fb99d53b0c0b')
+  // }).toArray().then((docs) => {
+  //   console.log('Todos:');
+  //   console.log(JSON.stringify(docs, undefined, 2));
+  // }, (err) => {
+  //   console.log('Unable to fetch Todo', err);
+  // });
+
+  // db.collection('Todos').find().count().then((count) => {
+  //   console.log('Todos:');
+  //   console.log('Todos count: ', count);
+  // }, (err) => {
+  //   console.log('Unable to fetch Todo', err);
+  // });
+
+  db.collection('Users').find({name: 'Catherine George'}).toArray().then((docs) => {
+    console.log('Users');
+    console.log(JSON.stringify(docs, undefined, 2));
+  }, (err) => {
+    console.log('Unable to fetch users', err);
+  });
+
+  // db.close();
+});
