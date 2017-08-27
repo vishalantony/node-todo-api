fc3faef78629bf6c3aa4088168cf485fcc83d60a
Vishal Antony <vishalantony@outlook.com>
2017-08-27 18:32:19 +0530

    Add models

---
+++ server/server.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const { mongoose } = require('./db/mongoose');
+const { Todo } = require('./models/todo');
+const { User }  = require('./models/user');
+
+var app = express();
+
+app.use(bodyParser.json());
+
+app.post('/todos', (req, res) => {
+  var todo = new Todo({
+    text: req.body.text
+  });
+  todo.save().then((data) => {
+    console.log('Success: ', data);
+    res.send(data);
+  }, (error) => {
+    console.log('Error: ', error);
+    res.status(400).send(error);
+  });
+});
+
+app.listen(3000, () => {
+  console.log('Started on port 3000');
+});
