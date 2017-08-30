2af02dfffad8ebe19881dd72fdf0a359758889bd
Vishal Antony <vishalantony@outlook.com>
2017-08-31 00:40:29 +0530

    Add test cases for DELETE /todos/:id route

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -106,3 +106,41 @@ describe('GET /todos/:id', () => {
       .expect(404, done);
   })
 });
+
+
+describe('DELETE /todos/:id', () => {
+  it('should remove a todo', (done) => {
+    var hexID = todos[1]._id.toHexString();
+    request(app)
+      .delete(`/todos/${hexID}`)
+      .expect(200)
+      .expect((res) => {
+        expect(res.body.todo._id).toBe(hexID);
+      })
+      .end((err, res) => {
+        if(err) {
+          return done(err);
+        }
+        Todo.findById(hexID).then((todo) => {
+          expect(todo).toNotExist();
+          done();
+        }).catch((err) => done(err));
+      });
+  });
+
+  it('should return 404 if not found', (done) => {
+    var hexID = (new ObjectID()).toHexString();
+    request(app)
+      .delete(`/todos/${hexID}`)
+      .expect(404)
+      .end(done);
+  });
+
+  it('should return 404 if object id is invalid', (done) => {
+    var hexID = 'fgjebdhwh8723uyhjwfv87tueyghjkuydrfchgvbjkity';
+    request(app)
+      .delete(`/todos/${hexID}`)
+      .expect(404)
+      .end(done);
+  });
+});
